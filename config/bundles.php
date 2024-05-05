dd4b5ccc8fd719f46b8b6748cda424403c818f5e
Marc Raes <contact@marcraes.fr>
2024-05-05 16:16:05 +0200

    Installation des bundles
    - `symfonycasts/verify-email-bundle`
    - `symfonycasts/reset-password-bundle`
    - `odolbeau/phone-number-bundle`
    - `liip/imagine-bundle`
    - `vich/uploader-bundle`
    - `twig/intl-extra`

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,9 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    Misd\PhoneNumberBundle\MisdPhoneNumberBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
